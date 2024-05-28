08c5b577aaf65e51c502c92327115f0f702094be
Jonas <pascalphelip@gmail.com>
2024-05-28 16:36:23 +0200

    navbar update change to stateful add corrections

--- Boule_de_Crystal/lib/main.dart
+++ Boule_de_Crystal/lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:boule_de_crystal/pages/event_page.dart';
 import 'package:boule_de_crystal/pages/home_page.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
@@ -5,14 +6,13 @@ import 'database/database_helper.dart';
 import 'models/zodiac_sign.dart';
 import 'models/chinese_zodiac_sign.dart';
 
-
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await initializeDatabase();
+ // await initializeDatabase();
   runApp(const MyApp());
 }
 
-Future<void> initializeDatabase() async {
+/* Future<void> initializeDatabase() async {
   final dbHelper = DatabaseHelper();
 
   // Signes du zodiaque
@@ -83,10 +83,24 @@ Future<void> initializeDatabase() async {
     await dbHelper.insertChineseZodiacSign(sign);
   }
 }
-
-
-class MyApp extends StatelessWidget {
+ */
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+
+  int _currentIndex = 0;
+
+  setCurrentIndex(int index) {
+    setState(() {
+      _currentIndex = index;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -99,12 +113,20 @@ class MyApp extends StatelessWidget {
           appBar: AppBar(
             title: const Text('Boule de Crystal'),
           ),
-          body: const HomePage(),
+          body: [
+            HomePage(), // redirect to HomePage(),
+            EventPage(), // redirect to EventPage(),
+            // add your new pages here Ingrid if you want so.
+          ][_currentIndex],
           bottomNavigationBar: LayoutBuilder(
             builder: (BuildContext context, BoxConstraints constraints) {
               return SizedBox(
                 height: constraints.maxHeight * 0.17, // 17% of screen height
                 child: BottomNavigationBar(
+                  // unselectedItemColor: Colors.grey, // Color of icons when not selected
+                  // selectedItemColor: Colors.blue, // Color of icons when selected
+                  currentIndex: _currentIndex,
+                  onTap: (index) => setCurrentIndex(index),
                   items: <BottomNavigationBarItem>[
                     BottomNavigationBarItem(
                       icon: SizedBox(
@@ -138,4 +160,6 @@ class MyApp extends StatelessWidget {
       ),
     );
   }
+
+
 }
