e4d9d7d24e2132bbb989eab3293915a6fafbe615
Gridou87 <grigridou87@gmail.com>
2024-05-30 15:32:15 +0200

    Calculate the love compatibility and displays it

--- Boule_de_Crystal/lib/pages/love_compatibility_page.dart
+++ Boule_de_Crystal/lib/pages/love_compatibility_page.dart
@@ -12,31 +12,35 @@ class _LoveCompatibilityPageState extends State<LoveCompatibilityPage> {
   String? _selectedSign2;
   String? _compatibilityResult;
 
-  final List<String> _signs = [
-    'Bélier',
-    'Taureau',
-    'Gémeaux',
-    'Cancer',
-    'Lion',
-    'Vierge',
-    'Balance',
-    'Scorpion',
-    'Sagittaire',
-    'Capricorne',
-    'Verseau',
-    'Poissons',
-  ];
+  final Map<String, int> _signsWithIds = {
+    'Bélier': 1,
+    'Taureau': 2,
+    'Gémeaux': 3,
+    'Cancer': 4,
+    'Lion': 5,
+    'Vierge': 6,
+    'Balance': 7,
+    'Scorpion': 8,
+    'Sagittaire': 9,
+    'Capricorne': 10,
+    'Verseau': 11,
+    'Poissons': 12,
+  };
 
   void _calculateCompatibility() {
-    if (_selectedSign1 != null && _selectedSign2 != null) {
-      setState(() {
-        _compatibilityResult = 'Compatibilité entre $_selectedSign1 et $_selectedSign2';
-      });
-    } else {
+    if (_selectedSign1 == null || _selectedSign2 == null) {
       setState(() {
         _compatibilityResult = 'Veuillez faire la sélection correcte des deux signes astrologiques';
       });
+      return;
     }
+
+    int id1 = _signsWithIds[_selectedSign1]!;
+    int id2 = _signsWithIds[_selectedSign2]!;
+    int compatibilityScore = (id1 + id2) * 4;
+    setState(() {
+      _compatibilityResult = 'Compatibilité entre $_selectedSign1 et $_selectedSign2 :\n$compatibilityScore%';
+    });
   }
 
   @override
@@ -58,7 +62,7 @@ class _LoveCompatibilityPageState extends State<LoveCompatibilityPage> {
                   _selectedSign1 = newValue;
                 });
               },
-              items: _signs.map<DropdownMenuItem<String>>((String value) {
+              items: _signsWithIds.keys.map<DropdownMenuItem<String>>((String value) {
                 return DropdownMenuItem<String>(
                   value: value,
                   child: Text(value),
@@ -74,7 +78,7 @@ class _LoveCompatibilityPageState extends State<LoveCompatibilityPage> {
                   _selectedSign2 = newValue;
                 });
               },
-              items: _signs.map<DropdownMenuItem<String>>((String value) {
+              items: _signsWithIds.keys.map<DropdownMenuItem<String>>((String value) {
                 return DropdownMenuItem<String>(
                   value: value,
                   child: Text(value),
@@ -91,9 +95,23 @@ class _LoveCompatibilityPageState extends State<LoveCompatibilityPage> {
             const SizedBox(height: 20),
             if (_compatibilityResult != null)
               Center(
-                child: Text(
-                  _compatibilityResult!,
-                  style: const TextStyle(fontSize: 16.0, fontWeight: FontWeight.bold),
+                child: Column(
+                  children: [
+                    Text(
+                      _compatibilityResult!.contains('Compatibilité')
+                          ? _compatibilityResult!.split(':').first
+                          : _compatibilityResult!,
+                      style: const TextStyle(fontSize: 18.0, fontWeight: FontWeight.bold),
+                    ),
+                    if (_compatibilityResult!.contains('Compatibilité'))
+                      const SizedBox(height: 5),
+                    if (_compatibilityResult!.contains('Compatibilité'))
+                      Text(
+                        _compatibilityResult!.split(':').last,
+                        textAlign: TextAlign.center,
+                        style: const TextStyle(fontSize: 24.0, fontWeight: FontWeight.bold, color: Colors.red),
+                      ),
+                  ],
                 ),
               ),
           ],
