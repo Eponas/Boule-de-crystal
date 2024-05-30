86f69a8d1960a1e8380d626e9580f24476e699bd
Gridou87 <grigridou87@gmail.com>
2024-05-30 19:08:30 +0200

    Using database to have the names of the sign in the two dropdowns and to calculate love compatibility between two signs

--- Boule_de_Crystal/lib/pages/love_compatibility_page.dart
+++ Boule_de_Crystal/lib/pages/love_compatibility_page.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import '../database/database_helper.dart';
+import '../models/zodiac_sign.dart';
 
 class LoveCompatibilityPage extends StatefulWidget {
   const LoveCompatibilityPage({super.key});
@@ -12,20 +14,14 @@ class _LoveCompatibilityPageState extends State<LoveCompatibilityPage> {
   String? _selectedSign2;
   String? _compatibilityResult;
 
-  final Map<String, int> _signsWithIds = {
-    'Bélier': 1,
-    'Taureau': 2,
-    'Gémeaux': 3,
-    'Cancer': 4,
-    'Lion': 5,
-    'Vierge': 6,
-    'Balance': 7,
-    'Scorpion': 8,
-    'Sagittaire': 9,
-    'Capricorne': 10,
-    'Verseau': 11,
-    'Poissons': 12,
-  };
+  final DatabaseHelper _databaseHelper = DatabaseHelper();
+  late Future<List<ZodiacSign>> _zodiacSignsFuture;
+
+  @override
+  void initState() {
+    super.initState();
+    _zodiacSignsFuture = _databaseHelper.getZodiacSigns();
+  }
 
   void _calculateCompatibility() {
     if (_selectedSign1 == null || _selectedSign2 == null) {
@@ -35,11 +31,16 @@ class _LoveCompatibilityPageState extends State<LoveCompatibilityPage> {
       return;
     }
 
-    int id1 = _signsWithIds[_selectedSign1]!;
-    int id2 = _signsWithIds[_selectedSign2]!;
-    int compatibilityScore = (id1 + id2) * 4;
-    setState(() {
-      _compatibilityResult = 'Compatibilité entre $_selectedSign1 et $_selectedSign2 :\n$compatibilityScore%';
+    final selectedSign1 = _zodiacSignsFuture.then((signs) => signs.firstWhere((sign) => sign.libelle == _selectedSign1));
+    final selectedSign2 = _zodiacSignsFuture.then((signs) => signs.firstWhere((sign) => sign.libelle == _selectedSign2));
+
+    selectedSign1.then((sign1) {
+      selectedSign2.then((sign2) {
+        int compatibilityScore = (sign1.id! + sign2.id!) * 4;
+        setState(() {
+          _compatibilityResult = 'Compatibilité entre ${sign1.libelle} et ${sign2.libelle} :\n$compatibilityScore%';
+        });
+      });
     });
   }
 
@@ -49,73 +50,88 @@ class _LoveCompatibilityPageState extends State<LoveCompatibilityPage> {
       appBar: AppBar(
         title: const Text('Compatibilité amoureuse'),
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            const Text('Entrez votre signe astrologique'),
-            DropdownButton<String>(
-              value: _selectedSign1,
-              onChanged: (String? newValue) {
-                setState(() {
-                  _selectedSign1 = newValue;
-                });
-              },
-              items: _signsWithIds.keys.map<DropdownMenuItem<String>>((String value) {
-                return DropdownMenuItem<String>(
-                  value: value,
-                  child: Text(value),
-                );
-              }).toList(),
-            ),
-            const SizedBox(height: 20),
-            const Text('Entrez son signe astrologique'),
-            DropdownButton<String>(
-              value: _selectedSign2,
-              onChanged: (String? newValue) {
-                setState(() {
-                  _selectedSign2 = newValue;
-                });
-              },
-              items: _signsWithIds.keys.map<DropdownMenuItem<String>>((String value) {
-                return DropdownMenuItem<String>(
-                  value: value,
-                  child: Text(value),
-                );
-              }).toList(),
-            ),
-            const SizedBox(height: 20),
-            Center(
-              child: ElevatedButton(
-                onPressed: _calculateCompatibility,
-                child: const Text('Calculer'),
-              ),
-            ),
-            const SizedBox(height: 20),
-            if (_compatibilityResult != null)
-              Center(
-                child: Column(
-                  children: [
-                    Text(
-                      _compatibilityResult!.contains('Compatibilité')
-                          ? _compatibilityResult!.split(':').first
-                          : _compatibilityResult!,
-                      style: const TextStyle(fontSize: 18.0, fontWeight: FontWeight.bold),
-                    ),
-                    if (_compatibilityResult!.contains('Compatibilité'))
-                      const SizedBox(height: 5),
-                    if (_compatibilityResult!.contains('Compatibilité'))
-                      Text(
-                        _compatibilityResult!.split(':').last,
-                        textAlign: TextAlign.center,
-                        style: const TextStyle(fontSize: 24.0, fontWeight: FontWeight.bold, color: Colors.red),
-                      ),
-                  ],
+      body: FutureBuilder<List<ZodiacSign>>(
+        future: _zodiacSignsFuture,
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const Center(child: CircularProgressIndicator());
+          } else if (snapshot.hasError) {
+            return const Center(child: Text('Erreur de chargement des signes astrologiques'));
+          } else if (!snapshot.hasData || snapshot.data!.isEmpty) {
+            return const Center(child: Text('Aucun signe astrologique trouvé'));
+          }
+
+          final zodiacSigns = snapshot.data!;
+
+          return Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                const Text('Entrez votre signe astrologique'),
+                DropdownButton<String>(
+                  value: _selectedSign1,
+                  onChanged: (String? newValue) {
+                    setState(() {
+                      _selectedSign1 = newValue;
+                    });
+                  },
+                  items: zodiacSigns.map<DropdownMenuItem<String>>((ZodiacSign sign) {
+                    return DropdownMenuItem<String>(
+                      value: sign.libelle,
+                      child: Text(sign.libelle),
+                    );
+                  }).toList(),
                 ),
-              ),
-          ],
-        ),
+                const SizedBox(height: 20),
+                const Text('Entrez son signe astrologique'),
+                DropdownButton<String>(
+                  value: _selectedSign2,
+                  onChanged: (String? newValue) {
+                    setState(() {
+                      _selectedSign2 = newValue;
+                    });
+                  },
+                  items: zodiacSigns.map<DropdownMenuItem<String>>((ZodiacSign sign) {
+                    return DropdownMenuItem<String>(
+                      value: sign.libelle,
+                      child: Text(sign.libelle),
+                    );
+                  }).toList(),
+                ),
+                const SizedBox(height: 20),
+                Center(
+                  child: ElevatedButton(
+                    onPressed: _calculateCompatibility,
+                    child: const Text('Calculer'),
+                  ),
+                ),
+                const SizedBox(height: 20),
+                if (_compatibilityResult != null)
+                  Center(
+                    child: Column(
+                      children: [
+                        Text(
+                          _compatibilityResult!.contains('Compatibilité')
+                              ? _compatibilityResult!.split(':').first
+                              : _compatibilityResult!,
+                          style: const TextStyle(fontSize: 18.0, fontWeight: FontWeight.bold),
+                        ),
+                        if (_compatibilityResult!.contains('Compatibilité'))
+                          const SizedBox(height: 5),
+                        if (_compatibilityResult!.contains('Compatibilité'))
+                          Text(
+                            _compatibilityResult!.split(':').last,
+                            textAlign: TextAlign.center,
+                            style: const TextStyle(fontSize: 24.0, fontWeight: FontWeight.bold, color: Colors.red),
+                          ),
+                      ],
+                    ),
+                  ),
+              ],
+            ),
+          );
+        },
       ),
     );
   }
